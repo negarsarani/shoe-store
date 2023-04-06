3443303cbe3d6dc599e5d1542a80d80223772f7b
Negar Sarani <negar.sarani2000@gmail.com>
2023-04-06 18:31:13 +0430

    page 2

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,12 @@
 import El from './library/El';
 import { onboarding } from './pages/onboarding';
+import { getStarted } from './pages/onboarding/getstarted';
 function App() {
   
      return El({
       element: 'div',
       className: 'w-full h-full font-Inter ',
-      child:onboarding()
+      child:getStarted()
     })
   
 }
--- my-project/src/pages/onboarding/getstarted/index.js
+++ my-project/src/pages/onboarding/getstarted/index.js
@@ -1,7 +1,37 @@
-import El from "@/library/El";
+import El from '@/library/El';
 
 export function getStarted() {
-   return El({
-    
-   }) 
-}  
+  return El({
+    element: 'div',
+    className:
+      "h-full w-full bg-red-300 bg-[url('/assets/img/Wallpaper-page2.png')] bg-cover bg-center text-white",
+    child: [
+      El({
+        element: 'div',
+        className: 'flex',
+        child: El({
+          element: 'h1',
+          // className: 'flex gap-2',
+          child: [
+            El({
+              element: 'span',
+              className: ' text-[40px]',
+              child: 'Welcome to 👋',
+            }),
+            El({
+              element: 'span',
+              className: ' text-[72px]',
+              child: 'Shoea',
+            }),
+          ],
+        }),
+      }),
+      El({
+        element: 'p',
+        className: '',
+        child:
+          'The best sneakers & shoes e-commerse app of the century for your fashion needs!',
+      }),
+    ],
+  });
+}
